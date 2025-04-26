3ccfacd0f8c774966ee7e0b790d77820959a3851
Leo Raymond <leo@MacBook-Air-7.local>
2025-04-26 19:20:45 +0200

    Add HelmetProvider to fix rendering error

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProfileProvider } from './context/UserProfileContext';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProfileProvider>
-      <App />
-    </UserProfileProvider>
+    <HelmetProvider>
+      <UserProfileProvider>
+        <App />
+      </UserProfileProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
